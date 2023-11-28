c7916043b4096ba673703b9ed9242bd34d2d00ce
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-11-28 19:59:26 +0600

    fixed cpu showing only one core

--- src/blazefetch_test.cpp
+++ src/blazefetch_test.cpp
@@ -9,6 +9,7 @@
 #include <getopt.h>
 #include <vector>
 #include <dirent.h>
+#include <unordered_map>
 
 
 #include <sys/shm.h>
@@ -354,19 +355,31 @@ std::string getCpuInfo() {
     FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
     if (cpuinfo) {
         char line[256];
-        int cpuCount = 0;
+        std::unordered_map<std::string, int> cpuModels;
 
         while (fgets(line, sizeof(line), cpuinfo) != NULL) {
             if (strstr(line, "model name")) {
-                cpuCount++;
                 char *model = strchr(line, ':') + 2;
                 model[strlen(model) - 1] = '\0'; // Remove the trailing newline
-                fclose(cpuinfo);
-                return "\033[95m" + std::string(CPU) + " \033[0m" + std::string(model) + " (x" + std::to_string(cpuCount) + ")";
+                cpuModels[std::string(model)]++;
             }
         }
 
         fclose(cpuinfo);
+
+        if (!cpuModels.empty()) {
+            std::string result = "\033[95m" + std::string(CPU) + " \033[0m";
+            
+            for (const auto& entry : cpuModels) {
+                result += entry.first + " (x" + std::to_string(entry.second) + "), ";
+            }
+
+            // Remove the trailing comma and space
+            result.pop_back();
+            result.pop_back();
+            
+            return result;
+        }
     }
 
     return "\033[95m" + std::string(CPU) + " \033[0mUnknown";
