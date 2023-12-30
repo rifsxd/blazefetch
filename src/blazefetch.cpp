3047272ecde3ef564511b0ce7f79ccb140444098
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-12-30 19:58:37 +0600

    fixed some typos

--- src/blazefetch.cpp
+++ src/blazefetch.cpp
@@ -806,9 +806,9 @@ void printHelp() {
               << "Options:\n"
               << "  -d, --daemon      Run as a daemon\n"
               << "  -g, --get <INFO>  Get and display specific information (e.g., OS, GPU)\n"
-              << "  -l, --live		  Launch live fetch instance\n"
+              << "  -l, --live        Launch live fetch instance\n"
               << "  -c, --clear       Clears the stored cache from memory\n"
-              << "  -r, --remove      Removes the lock file incase if somethign went wrong\n"
+              << "  -r, --remove      Removes the lock file incase if something went wrong\n"
               << "  -v, --version     Show version information\n"
               << "  -k, --kill        Kill the daemon process\n"
               << "  -h, --help        Show this help message\n\n";
